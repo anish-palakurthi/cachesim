549d0e6282bf13a55b6c9dbacaf3dd5ebffd26bb
Anish-Palakurthi <anish.palakurthi@utexas.edu>
2024-04-20 14:42:23 -0500

    added new dins for dinero runs

--- d4-7/testing/conversion.py
+++ d4-7/testing/conversion.py
@@ -1,21 +1,39 @@
+import os
 # Specify the path to your text file
-file_path = 'testing/047.tomcatv.din'
-output_file = 'testing/foursTwo.din'
+source_dir_path = "/Users/anishpalakurthi/Desktop/dineroProj/manualSim/Spec_Benchmark"
+output_dir_path = "/Users/anishpalakurthi/Desktop/dineroProj/d4-7/testing/preprocessed_dins"
 
-map = {0: 'r', 1: 'w', 2: 'i'}
-# Open the file in read mode
-res = []
-with open(file_path, 'r') as file:
-    # Iterate through each line in the file
-    for line in file:
-        # Strip any leading/trailing whitespace
-        chunks = line.strip().split(' ')
-        if (chunks[0] == '3'):
-            continue
+def preprocess(file_path, output_file):
 
-        chunks[0] = map[int(chunks[0])]
-        chunks[2] = '4'
-        res.append(" ".join(chunks))
+    map = {0: 'r', 1: 'w', 2: 'i'}
+    # Open the file in read mode
+    res = []
+    with open(file_path, 'r') as file:
+        # Iterate through each line in the file
+        for line in file:
+            # Strip any leading/trailing whitespace
+            chunks = line.strip().split(' ')
+            if (chunks[0] == '3'):
+                continue
 
-with open(output_file, 'w') as file:
-    file.write("\n".join(res))
+            chunks[0] = map[int(chunks[0])]
+            chunks[2] = '4'
+            res.append(" ".join(chunks))
+
+    with open(output_file, 'w') as file:
+        file.write("\n".join(res))
+
+
+def main():
+    # Iterate through each file in the directory
+    for file_name in os.listdir(source_dir_path):
+        # Check if the file is a text file
+        if file_name.endswith('.din'):
+            # Preprocess the file
+            source_file_path = os.path.join(source_dir_path, file_name)
+            output_file_path = os.path.join(output_dir_path, file_name)
+            preprocess(source_file_path, output_file_path)
+
+
+if __name__ == '__main__':
+    main()
